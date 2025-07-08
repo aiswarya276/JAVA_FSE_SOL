f1029f70170843189dedb5b0bd004360a3bc3f8a
aiswarya276 <aiswaryapatra276@gmail.com>
2025-07-09 00:21:41 +0530

    Create LibraryManagementApp.java

---
+++ SPRING_CORE_MAVEN/Q1/LobraryManagementApp.java
@@ -0,0 +1,19 @@
+package com.library;
+
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryManagementApp {
+
+    public static void main(String[] args) {
+        // Load Spring context
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get the BookService bean
+        BookService bookService = (BookService) context.getBean("bookService");
+
+        // Use the service to add a book
+        bookService.addBook("Effective Java");
+    }
+}
